6286ea004635b9ddc40460892b770fcd9516375a
Mohammed BENKHALID <benkhalid.mo@gmail.com>
2020-03-27 14:52:52 +0100

    file read, write and occurrence file 3 with src files

---
+++ src/src/com/hemebiotech/analytics/WriteSymptomDataFromFile.java
@@ -0,0 +1,39 @@
+package src.com.hemebiotech.analytics;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WriteSymptomDataFromFile {
+
+	
+	public Map<String, Integer> transfert() throws IOException{
+
+	
+	//var source = Paths.get("C:/Users/campus/eclipse-workspace/P2_Ocr/src/symptoms.txt");
+	//var fis = Files.newInputStream(source);	
+		
+		Map<String, Integer>write = new HashMap();
+		String writeString;
+
+			
+			var source = Paths.get(config.inputLink);
+	        var dest = Paths.get(config.outputLink);
+	        
+	        var fis = Files.newInputStream(source);
+	        var fos = Files.newOutputStream(dest);
+	        
+	        byte[] buffer = new byte[1024];
+	        int length;
+	        while ((length = fis.read(buffer)) > 0) {
+	        	fos.write(buffer, 0, length);
+	        	
+	        }
+			return null;
+
+	            
+	        }
+}
---
+++ src/src/com/hemebiotech/analytics/config.java
@@ -0,0 +1,16 @@
+package src.com.hemebiotech.analytics;
+
+
+public class config {
+
+	public static String getInputlink() {
+		return inputLink;
+	}
+	public static String getOutputlink() {
+		return outputLink;
+	}
+	public static final String inputLink = "C:/Users/campus/eclipse-workspace/P2_Ocr/src/symptoms.txt";
+	public static final String outOccurences = "C:/Users/campus/eclipse-workspace/P2_Ocr/src/occurences.out";
+	public static final String outputLink = "C:/Users/campus/eclipse-workspace/P2_Ocr/src/results.out"; //results.out
+	
+}
